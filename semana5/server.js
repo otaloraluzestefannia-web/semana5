8e3311ae677dd2e7052cb21b3817887f86908533
otaloraluzestefannia-web <otaloraluzestefannia@gmail.com>
2026-05-29 20:46:20 +0000

    semana5

--- semana5/server.js
+++ semana5/server.js
@@ -19,6 +19,7 @@ app.get('/mensaje/:nombre', (req, res) => {
 app.listen(3000, () => {
   console.log('Servidor ejecutándose en puerto 3000');
 });
+
 app.post('/reporte', (req, res) => {
   const mensaje = req.body.mensaje;
   res.json({
